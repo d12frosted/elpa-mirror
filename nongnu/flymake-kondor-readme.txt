14084c478dc4d6b450e0a56318b4ac9a17bf03da
Golem <golem@d12frosted.io>
2021-10-23 12:09:45 +0000

    snapshot 23/10/21 12:09:45

--- nongnu/flymake-kondor-readme.txt
+++ nongnu/flymake-kondor-readme.txt
@@ -1,50 +1,73 @@
-# -*- mode:org; ispell-dictionary:"en_GB"  -*-
-#+TITLE: flymake backend clj-kondo
-#+AUTHOR: https://turbocafe.keybase.pub
-
-this  package  integrates  clj-kondo  a  clojure  linter  into  emacs'
-flymake;  to use  this  package get  clj-kondo following  [[https://github.com/borkdude/clj-kondo/blob/master/doc/install.md][installation
-instructions]]; then proceed with your preferred way of adding packages
-
-* melpa
-
-#+BEGIN_SRC emacs-lisp
-(use-package flymake-kondor
-  :ensure flymake-quickdef
-  :hook (clojure-mode . flymake-kondor-setup))
-#+END_SRC
-
-* github
-
-#+BEGIN_SRC emacs-lisp
-(el-get-bundle flymake-kondor
-               :url "https://raw.githubusercontent.com/turbo-cafe/flymake-kondor/master/flymake-kondor.el"
-               (add-hook 'clojure-mode-hook #'flymake-kondor-setup))
-#+END_SRC
-
-* local
-
-#+BEGIN_SRC emacs-lisp
-(add-to-list 'load-path "~/path/to/flymake-kondor")
-(require "flymake-kondor")
-(add-hook 'clojure-mode-hook #'flymake-kondor-setup)
-#+END_SRC
+		      ━━━━━━━━━━━━━━━━━━━━━━━━━━━
+		       FLYMAKE BACKEND CLJ-KONDO
+		      ━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 
-* note about flymake
+Table of Contents
+─────────────────
 
-to start linting  activate =M-x flymake-mode= in  clojure buffer; even
-better assign hook  and keys so you could navigate  to prev/next error
-in buffer instantly
+1. MELPA
+2. GitHub
+3. Local
+4. Note about Flymake
 
-#+BEGIN_SRC emacs-lisp
-(use-package flymake
-  :ensure nil
-  :bind (([f8] . flymake-goto-next-error)
-         ([f7] . flymake-goto-prev-error))
-  :hook (prog-mode . (lambda () (flymake-mode t)))
-  :config (remove-hook 'flymake-diagnostic-functions #'flymake-proc-legacy-flymake))
-#+END_SRC
 
-there's [[https://github.com/borkdude/flycheck-clj-kondo][sister project]] that integrates  clj-kondo into flycheck
+This package integrates clj-kondo a Clojure linter into Emacs' Flymake;
+to use it get clj-kondo following [installation instructions]; then
+proceed with your preferred way of adding packages.
 
+
+[installation instructions]
+<https://github.com/borkdude/clj-kondo/blob/master/doc/install.md>
+
+
+1 MELPA
+═══════
+
+  ┌────
+  │ (use-package flymake-kondor
+  │   :ensure t
+  │   :hook (clojure-mode . flymake-kondor-setup))
+  └────
+
+
+2 GitHub
+════════
+
+  ┌────
+  │ (el-get-bundle flymake-kondor
+  │ 	       :url "https://raw.githubusercontent.com/turbo-cafe/flymake-kondor/master/flymake-kondor.el"
+  │ 	       (add-hook 'clojure-mode-hook #'flymake-kondor-setup))
+  └────
+
+
+3 Local
+═══════
+
+  ┌────
+  │ (add-to-list 'load-path "~/path/to/flymake-kondor")
+  │ (require "flymake-kondor")
+  │ (add-hook 'clojure-mode-hook #'flymake-kondor-setup)
+  └────
+
+
+4 Note about Flymake
+════════════════════
+
+  To start linting activate `M-x flymake-mode' in a Clojure buffer; even
+  better assign hook and keys so you could navigate to the previous or
+  next error in the buffer instantly.
+
+  ┌────
+  │ (use-package flymake
+  │   :ensure nil
+  │   :bind (([f8] . flymake-goto-next-error)
+  │ 	 ([f7] . flymake-goto-prev-error))
+  │   :hook (prog-mode . (lambda () (flymake-mode t)))
+  │   :config (remove-hook 'flymake-diagnostic-functions #'flymake-proc-legacy-flymake))
+  └────
+
+  There is a [sister project] that integrates clj-kondo into Flycheck.
+
+
+[sister project] <https://github.com/borkdude/flycheck-clj-kondo>
--- stable-melpa/flymake-kondor-20211023.739.el
+++ stable-melpa/flymake-kondor-20211023.739.el
@@ -4,9 +4,9 @@
 ;;
 ;; Author: https://turbocafe.keybase.pub
 ;; Created: 3 November 2019
-;; Version: 0.1.0
-;; Package-Version: 20210814.1303
-;; Package-Commit: 0058ef5167142a521825e824aced6229e4898ae9
+;; Version: 0.1.2
+;; Package-Version: 20211023.739
+;; Package-Commit: 470228acfaed703198bbb04198461ef9a44821b0
 ;; Package-Requires: ((emacs "26.1"))
 ;; URL: https://github.com/turbo-cafe/flymake-kondor
 ;;; Commentary:
@@ -61,31 +61,32 @@
         :command `("clj-kondo" "--lint" "-" "--lang" ,lang)
         :sentinel
         (lambda (proc _event)
-          (when (eq 'exit (process-status proc))
-            (unwind-protect
-                (if (with-current-buffer source (eq proc flymake-kondor--flymake-proc))
-                    (with-current-buffer (process-buffer proc)
-                      (goto-char (point-min))
-                      (cl-loop
-                       while (search-forward-regexp
-                              "^.+:\\([[:digit:]]+\\):\\([[:digit:]]+\\): \\([[:alpha:]]+\\): \\(.+\\)$"
-                              nil t)
-                       for lnum = (string-to-number (match-string 1))
-                       for lcol = (string-to-number (match-string 2))
-                       for type = (let ((severity (match-string 3)))
-                                    (cond
-                                     ((string= severity "error") :error)
-                                     ((string= severity "warning") :warning)
-                                     ((string= severity "info") :note)
-                                     (t :note)))
-                       for msg = (match-string 4)
-                       for (beg . end) = (flymake-diag-region source lnum lcol)
-                       collect (flymake-make-diagnostic source beg end type msg)
-                       into diags
-                       finally (funcall report-fn diags)))
-                  (flymake-log :warning "Canceling obsolete check %s"
-                               proc))
-              (kill-buffer (process-buffer proc)))))))
+          (let ((status (process-status proc)))
+            (when (or (eq status 'exit) (eq status 'signal))
+              (unwind-protect
+                  (if (with-current-buffer source (eq proc flymake-kondor--flymake-proc))
+                      (with-current-buffer (process-buffer proc)
+                        (goto-char (point-min))
+                        (cl-loop
+                         while (search-forward-regexp
+                                "^.+:\\([[:digit:]]+\\):\\([[:digit:]]+\\): \\([[:alpha:]]+\\): \\(.+\\)$"
+                                nil t)
+                         for lnum = (string-to-number (match-string 1))
+                         for lcol = (string-to-number (match-string 2))
+                         for type = (let ((severity (match-string 3)))
+                                      (cond
+                                       ((string= severity "error") :error)
+                                       ((string= severity "warning") :warning)
+                                       ((string= severity "info") :note)
+                                       (t :note)))
+                         for msg = (match-string 4)
+                         for (beg . end) = (flymake-diag-region source lnum lcol)
+                         collect (flymake-make-diagnostic source beg end type msg)
+                         into diags
+                         finally (funcall report-fn diags)))
+                    (flymake-log :warning "Canceling obsolete check %s"
+                                 proc))
+                (kill-buffer (process-buffer proc))))))))
       (process-send-region flymake-kondor--flymake-proc (point-min) (point-max))
       (process-send-eof flymake-kondor--flymake-proc))))
 
