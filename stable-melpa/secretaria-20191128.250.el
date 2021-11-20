f9e0a52cc22ec1ff19de3ad31be53e305b26e552
Golem <golem@d12frosted.io>
2021-11-20 04:03:27 +0000

    snapshot 20/11/21 04:03:27

--- stable-melpa/secretaria-20191128.250.el
+++ stable-melpa/secretaria-20191128.250.el
@@ -4,8 +4,10 @@
 
 ;; Author: Jorge Araya Navarro <jorge@esavara.cr>
 ;; Keywords: org, convenience
+;; Package-Commit: 03986130a2ada1fa952d45e83536729f20230fcf
 ;; Package-Requires: ((emacs "24.4") (alert "1.2") (s "1.12") (f "0.20.0") (org "9"))
-;; Package-Version: 0.2.12
+;; Package-Version: 20191128.250
+;; Package-X-Original-Version: 0.2.12
 ;; Homepage: https://gitlab.com/shackra/secretaria
 
 ;; This file is not part of GNU Emacs.
