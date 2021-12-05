460707ce6ce76b5821de2da6cc76acc885d23f5c
Golem <golem@d12frosted.io>
2021-12-05 08:03:22 +0000

    snapshot 05/12/21 08:03:22

--- melpa/cmake-mode-20210104.1831.el
+++ melpa/cmake-mode-20210104.1831.el
@@ -2,7 +2,7 @@
 
 ;; Package-Requires: ((emacs "24.1"))
 ;; Package-Version: 20210104.1831
-;; Package-Commit: b00cb8f40530feefa245c92944da5a48d8ed804c
+;; Package-Commit: a54f18ff36f0dc47d565c111785515bc5e287c24
 
 ; Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 ; file Copyright.txt or https://cmake.org/licensing for details.
--- stable-melpa/scad-mode-20200830.301.el
+++ stable-melpa/scad-mode-20200830.301.el
@@ -6,7 +6,7 @@
 ;; Modified:   28 Jun 2020
 ;; Keywords:   languages
 ;; Package-Version: 20200830.301
-;; Package-Commit: a6a29b2cd1f2fc4578223fb7aa385a9280ebc7b9
+;; Package-Commit: 602f154293026e0c23483e4ca52f353a2b2a3d54
 ;; URL:        https://raw.github.com/openscad/openscad/master/contrib/scad-mode.el
 ;; Version:    92.0
 
