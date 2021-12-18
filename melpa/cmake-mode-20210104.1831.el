29465cd7503c3b3b23d1160ad60abf924641fdb4
Golem <golem@d12frosted.io>
2021-12-18 08:03:38 +0000

    snapshot 18/12/21 08:03:37

--- melpa/cmake-mode-20210104.1831.el
+++ melpa/cmake-mode-20210104.1831.el
@@ -2,7 +2,7 @@
 
 ;; Package-Requires: ((emacs "24.1"))
 ;; Package-Version: 20210104.1831
-;; Package-Commit: 3930c354f8d3669234c783505bc84dfe7ee9dcdb
+;; Package-Commit: 1fdff77fa52e94040a9b0bb57a9f1eb393c2b16b
 
 ; Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 ; file Copyright.txt or https://cmake.org/licensing for details.
