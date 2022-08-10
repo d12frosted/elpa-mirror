4e0c21428dd2858e1659a8e965de914bb6ec7f80
Golem <golem@d12frosted.io>
2022-08-10 08:10:00 +0000

    snapshot 10/08/22 08:10:00

--- melpa/cmake-mode-20220617.1532.el
+++ melpa/cmake-mode-20220617.1532.el
@@ -2,7 +2,7 @@
 
 ;; Package-Requires: ((emacs "24.1"))
 ;; Package-Version: 20220617.1532
-;; Package-Commit: f7a13beaa4e7ef262004e37997ffc5d856fc26f2
+;; Package-Commit: 29115e706aec3007a1b651ceed936e94f69de858
 
 ; Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 ; file Copyright.txt or https://cmake.org/licensing for details.
--- stable-melpa/thrift-20200212.1903.el
+++ stable-melpa/thrift-20200212.1903.el
@@ -2,7 +2,7 @@
 
 ;; Keywords: languages
 ;; Package-Version: 20200212.1903
-;; Package-Commit: ab2c2896b54290f8d515962d84612190be7151d0
+;; Package-Commit: ca8f46711d0e27476259b36fddac87df4d07cf37
 ;; Package-Requires: ((emacs "24"))
 
 ;; Licensed to the Apache Software Foundation (ASF) under one
