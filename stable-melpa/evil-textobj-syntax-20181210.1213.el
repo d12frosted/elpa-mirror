486cb8f836dcf3f36731b433a40965e51ac855c9
Golem <golem@d12frosted.io>
2022-09-09 16:09:36 +0000

    snapshot 09/09/22 16:09:36

--- stable-melpa/evil-textobj-syntax-20181210.1213.el
+++ stable-melpa/evil-textobj-syntax-20181210.1213.el
@@ -1,6 +1,8 @@
 ;;; evil-textobj-syntax.el --- Provides syntax text objects. -*- lexical-binding: t; -*-
 
 ;; URL: https://github.com/laishulu/evil-textobj-syntax
+;; Package-Version: 20181210.1213
+;; Package-Commit: 2d9ba8c75c754b409aea7469f46a5cfa52a872f3
 ;; Created: November 1, 2018
 ;; Keywords: evil, syntax, highlight, text-object
 ;; Package-Requires: ((names "0.5") (emacs "24") (evil "0"))
