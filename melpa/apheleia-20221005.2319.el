60d8e77afcf12dfbc1110bd80c838a47ce181d70
Golem <golem@d12frosted.io>
2022-10-06 04:24:01 +0000

    snapshot 06/10/22 04:24:00

--- melpa/apheleia-20221005.2319.el
+++ melpa/apheleia-20221005.2319.el
@@ -6,8 +6,8 @@
 ;; Created: 7 Jul 2019
 ;; Homepage: https://github.com/raxod502/apheleia
 ;; Keywords: tools
-;; Package-Version: 20220915.208
-;; Package-Commit: 7aa46ba84f06251f280d226f98cb06ef83a0a697
+;; Package-Version: 20221005.2319
+;; Package-Commit: aae79b7b2a1a7bc905b005ec3f43feb56918b93d
 ;; Package-Requires: ((emacs "26"))
 ;; SPDX-License-Identifier: MIT
 ;; Version: 3.0
@@ -805,11 +805,11 @@ machine from the machine file is available on"))
         (let ((input-fname (apheleia--strip-remote input-fname)))
           (setq command (mapcar (lambda (arg)
                                   (if (memq arg '(input inplace))
-                                      input-fname
+                                      (progn
+                                        (setq output-fname input-fname)
+                                        input-fname)
                                     arg))
-                                command)))
-        (when (memq 'inplace command)
-          (setq output-fname input-fname)))
+                                command))))
       (when (memq 'output command)
         (setq output-fname (apheleia--make-temp-file run-on-remote "apheleia"))
         (let ((output-fname (apheleia--strip-remote output-fname)))
@@ -873,9 +873,9 @@ purposes."
            (lambda (stdout)
              (when output-fname
                ;; Load output-fname contents into the stdout buffer.
-               (erase-buffer)
-               (insert-file-contents-literally output-fname))
-
+               (with-current-buffer stdout
+                 (erase-buffer)
+                 (insert-file-contents-literally output-fname)))
              (funcall callback stdout))
            :ensure
            (lambda ()
