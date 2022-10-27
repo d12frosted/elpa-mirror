bd125d9d42c238df1360a73720868d9f2c7c2e6a
Golem <golem@d12frosted.io>
2022-10-27 20:10:49 +0000

    snapshot 27/10/22 20:10:49

--- stable-melpa/unicode-emoticons-20150204.1108.el
+++ stable-melpa/unicode-emoticons-20150204.1108.el
@@ -4,6 +4,8 @@
 
 ;; Author: Gunther Hagleitner
 ;; Version: 0.1
+;; Package-Version: 20150204.1108
+;; Package-Commit: 52a09955c2afc1807c0f37f1467ccfc1e1da690a
 ;; Keywords: games, entertainment, comms
 ;; URL: https://github.com/hagleitn/unicode-emoticons
 ;; Package-Requires: ()
