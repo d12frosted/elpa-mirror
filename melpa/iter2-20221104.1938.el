466756fd086453e1fb7fc97e877e588a6570633d
Golem <golem@d12frosted.io>
2022-11-05 00:29:47 +0000

    snapshot 05/11/22 00:29:47

--- melpa/iter2-20221104.1938.el
+++ melpa/iter2-20221104.1938.el
@@ -4,9 +4,9 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    1.4snapshot
-;; Package-Version: 20220826.1344
-;; Package-Commit: 0cfd73224557d3b8282f4f5b24cbbfa4e0120184
+;; Version:    1.5snapshot
+;; Package-Version: 20221104.1938
+;; Package-Commit: 5ea6ba6effc4b71e7a4aed16b3f42408f9064c01
 ;; Keywords:   elisp, extensions
 ;; Homepage:   https://github.com/doublep/iter2
 ;; Package-Requires: ((emacs "25.1"))
@@ -43,8 +43,8 @@
 (require 'subr-x)
 
 (require 'bytecomp)
-;; See its usage below.
-(declare-function 'byte-compile-not-lexical-var-p "bytecomp")
+;; See the usage below.
+(defvar byte-compile-bound-variables)
 
 
 (defgroup iter2 nil
@@ -168,6 +168,20 @@ See `iter2-defun' for details."
 
 (defun iter2--literal-or-variable-p (x)
   (or (atom x) (memq (car x) '(quote function))))
+
+;; During byte-compilation, function `special-variable-p' won't give intended results for
+;; variables declared in the file being compiled.  E.g. byte-compiler itself used (pre-29)
+;; to use `byte-compile-not-lexical-var-p' to decide how to compile let-bindings.  Of
+;; course, they had to remove the function, even if it wass not private according to Elisp
+;; naming conventions.  It seems we still can use variable `byte-compile-bound-variables'
+;; at least.
+(defun iter2--special-variable-p (var &optional warn)
+  (cond ((boundp 'byte-compile-bound-variables)
+         (or (special-variable-p var) (memq var byte-compile-bound-variables)))
+        (t
+         (when warn
+           (warn "Variable `byte-compile-bound-variables' is missing, special variable bindings in byte-compiled `iter2-defun' might misbehave"))
+         (special-variable-p var))))
 
 (defmacro iter2--convert-progn (forms)
   (declare (debug (form)))
@@ -420,14 +434,7 @@ See `iter2-defun' for details."
                             value value-form))
                      (_
                       (error "Erroneous binding %S" binding)))
-                   ;; During byte-compilation, function `special-variable-p' won't give intended results for
-                   ;; variables declared in the file being compiled.  E.g. byte-compiler itself uses
-                   ;; `byte-compile-not-lexical-var-p' to decided, how to compile let-bindings.  The function
-                   ;; is not private according to Elisp naming conventions, but it's not unthinkable they just
-                   ;; drop it in a future version, so check before calling it.
-                   (let ((special (if (fboundp 'byte-compile-not-lexical-var-p)
-                                      (byte-compile-not-lexical-var-p var)
-                                    (special-variable-p var)))
+                   (let ((special (iter2--special-variable-p var))
                          (literal (iter2--literalp value)))
                      (cond (literal
                             (push binding converted-bindings)
@@ -897,8 +904,8 @@ See `iter2-defun' for details."
            (warn "Compatibility of `iter2' with `generator' package appears broken; please report this to maintainer (Emacs version: %s)" (emacs-version)))
         t))
 
-(unless (fboundp 'byte-compile-not-lexical-var-p)
-  (warn "Function `byte-compile-not-lexical-var-p' is missing, special variable bindings in byte-compiled `iter2-defun' might misbehave"))
+;; Issue a warning if compatibility is broken yet again.
+(iter2--special-variable-p nil t)
 
 
 ;; Work around missing Edebug specification for `iter-do' macro on older Emacs versions.
