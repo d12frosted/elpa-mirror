35114bc88046f74733c4333c025e774df6dca024
Golem <golem@d12frosted.io>
2022-12-02 08:10:09 +0000

    snapshot 02/12/22 08:10:08

--- melpa/guide-key-20150108.635.el
+++ melpa/guide-key-20150108.635.el
@@ -4,6 +4,8 @@
 
 ;; Author: Tsunenobu Kai <kai2nenobu@gmail.com>
 ;; URL: https://github.com/kai2nenobu/guide-key
+;; Package-Version: 20150108.635
+;; Package-Commit: 9236d287a7272e307fb941237390a96037c8c0a2
 ;; Version: 1.2.5
 ;; Package-Requires: ((dash "2.10.0") (popwin "0.3.0") (s "1.9.0"))
 ;; Keywords: help convenience
