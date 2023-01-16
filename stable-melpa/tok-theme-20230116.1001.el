bd6a92247c311f2a4d154b88ced0d03bc630fa3f
Golem <golem@d12frosted.io>
2023-01-16 20:10:26 +0000

    snapshot 16/01/23 20:10:26

---
+++ stable-melpa/tok-theme-20230116.1001.el
@@ -0,0 +1,148 @@
+;;; tok-theme.el --- Minimal, calm and dark theme for Emacs -*- lexical-binding: t; -*-
+
+;; Author: Topi Kettunen <topi@topikettunen.com>
+;; URL: https://github.com/topikettunen/tok-theme
+;; Package-Version: 20230116.1001
+;; Package-Commit: b31743c9f7f3c1589be646c3972cc163b072c007
+;; Version: 0.1
+;; Package-Requires: ((emacs "26.1"))
+
+;; This is free and unencumbered software released into the public domain.
+;;
+;; Anyone is free to copy, modify, publish, use, compile, sell, or
+;; distribute this software, either in source code form or as a compiled
+;; binary, for any purpose, commercial or non-commercial, and by any
+;; means.
+;;
+;; In jurisdictions that recognize copyright laws, the author or authors
+;; of this software dedicate any and all copyright interest in the
+;; software to the public domain. We make this dedication for the benefit
+;; of the public at large and to the detriment of our heirs and
+;; successors. We intend this dedication to be an overt act of
+;; relinquishment in perpetuity of all present and future rights to this
+;; software under copyright law.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;; IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+;; OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+;; ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;; OTHER DEALINGS IN THE SOFTWARE.
+;;
+;; For more information, please refer to <https://unlicense.org>
+
+;; This file is not part of Emacs.
+
+;;; Commentary:
+
+;; Tok is a minimal, calm and dark theme for Emacs.
+
+;;; Code:
+
+(deftheme tok
+  "Minimal, calm and dark theme for Emacs")
+
+(let ((class '((class color) (min-colors 89)))
+      (dark-grey "#141414") (grey "#525252") (light-grey "#a3a3a3")
+      (fg "#cccccc") (bg "#000"))
+  (custom-theme-set-faces
+   'tok
+   ;; In case you're using this theme in terminal, let the terminal
+   ;; emulator define these.
+   (when (display-graphic-p)
+     `(cursor ((,class (:background "red")))))
+   (when (display-graphic-p) ; Have to call `when' here due to reasons.
+     `(default ((,class (:foreground ,fg :background ,bg)))))
+
+   ;; Basic faces
+   `(highlight ((,class (:background ,dark-grey))))
+   `(region ((,class (,@(and (>= emacs-major-version 27) '(:extend t))
+                      :background ,grey))))
+   `(secondary-selection ((,class (:inherit region))))
+   `(trailing-whitespace ((,class (:inherit error))))
+   `(error ((,class (:weight bold :foreground "firebrick1"))))
+   `(warning ((,class (:weight bold :foreground "DarkOrange"))))
+   `(success ((,class (:weight bold :foreground "Green1"))))
+   `(minibuffer-prompt ((,class (:foreground ,light-grey))))
+   `(fringe ((t (nil))))
+   `(button ((,class (:underline t))))
+
+   ;; Line-numbes
+   `(line-number ((,class (:foreground ,grey))))
+   `(line-number-current-line ((,class (:inherit highlight))))
+
+   ;; Mode-line
+   `(mode-line ((,class (:foreground "#fff" :background "grey5" :box (:line-width -1 :style released-button)))))
+   (when (>= emacs-major-version 29)
+     `(mode-line-active ((,class (:inherit mode-line)))))
+   `(mode-line-inactive ((,class (:weight light :foreground "grey90" :background "grey10"))))
+   `(mode-line-highlight ((t (nil))))
+   `(mode-line-emphasis ((,class (:weight bold))))
+   `(mode-line-buffer-id ((,class (:weight bold))))
+
+   ;; Header
+   `(header-line ((,class (:inherit mode-line-inactive :box nil))))
+
+   ;; Font-lock
+   `(font-lock-comment-face ((,class (:foreground "#fff" :weight bold))))
+   `(font-lock-comment-delimiter-face ((,class (:inherit font-lock-comment-face))))
+   `(font-lock-string-face ((,class (:foreground "#fff"))))
+   `(font-lock-doc-face ((, class(:inherit font-lock-comment-face))))
+   `(font-lock-doc-markup-face ((t (nil))))
+   `(font-lock-keyword-face ((t (nil))))
+   `(font-lock-builtin-face ((t (nil))))
+   `(font-lock-function-name-face ((t (nil))))
+   `(font-lock-variable-name-face ((t (nil))))
+   `(font-lock-type-face ((t (nil))))
+   `(font-lock-constant-face ((t (nil))))
+   `(font-lock-warning-face ((t (nil))))
+   `(font-lock-negation-char-face ((t (nil))))
+   `(font-lock-preprocessor-face ((,class (:inherit font-lock-comment-face))))
+   `(font-lock-regexp-grouping-backslash ((t (nil))))
+   `(font-lock-regexp-grouping-construct ((t (nil))))
+
+   ;; Dired
+   `(dired-directory ((,class (:foreground "cyan"))))
+   `(dired-symlink ((,class (:foreground "magenta"))))
+   `(dired-broken-symlink ((,class (:foreground "red"))))
+
+   ;; ERC
+   `(erc-timestamp-face ((,class (:foreground nil))))
+
+   ;; sh
+   `(sh-heredoc ((t (nil))))
+   `(sh-quoted-exec ((t (nil))))
+
+   ;; Outline
+   `(outline-1 ((,class (:foreground ,light-grey))))
+   `(outline-2 ((,class (:inherit outline-1))))
+   `(outline-3 ((,class (:inherit outline-1))))
+   `(outline-4 ((,class (:inherit outline-1))))
+   `(outline-5 ((,class (:inherit outline-1))))
+   `(outline-6 ((,class (:inherit outline-1))))
+   `(outline-7 ((,class (:inherit outline-1))))
+   `(outline-8 ((,class (:inherit outline-1))))
+
+   ;; Terraform
+   `(terraform--resource-name-face ((t (nil))))
+   `(terraform--resource-type-face ((t (nil))))
+
+   ;; Markdown
+   `(markdown-header-face ((,class (:inherit outline-1))))
+   `(markdown-header-delimiter-face ((t (nil))))
+   `(markdown-metadata-key-face ((,class (:inherit font-lock-comment-face))))
+   `(markdown-metadata-value-face ((,class (:inherit font-lock-comment-face))))))
+
+;;;###autoload
+(when (and (boundp 'custom-theme-load-path) load-file-name)
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'tok)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+
+;;; tok-theme.el ends here
--- stable-melpa/tok-theme-readme.txt
+++ stable-melpa/tok-theme-readme.txt
@@ -1,1 +1,1 @@
-Tok is a minimal and calm theme with dark and saffron color scheme.
+Tok is a minimal, calm and dark theme for Emacs.
