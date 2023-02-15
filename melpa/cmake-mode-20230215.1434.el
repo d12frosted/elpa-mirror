685d15e522509418f2b24207b61bfae70e74c833
Golem <golem@d12frosted.io>
2023-02-15 16:10:48 +0000

    snapshot 15/02/23 16:10:48

--- melpa/cmake-mode-20230215.1434.el
+++ melpa/cmake-mode-20230215.1434.el
@@ -1,8 +1,8 @@
 ;;; cmake-mode.el --- major-mode for editing CMake sources
 
 ;; Package-Requires: ((emacs "24.1"))
-;; Package-Version: 20230209.1921
-;; Package-Commit: 29bcbcab4fe3750d7529f49e9d71768c76a23a47
+;; Package-Version: 20230215.1434
+;; Package-Commit: 037975ef69a0e889f9c17e5e6e6fb87111dfd9df
 
 ; Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 ; file Copyright.txt or https://cmake.org/licensing for details.
--- stable-melpa/helm-lean-20210305.1705.el
+++ stable-melpa/helm-lean-20210305.1705.el
@@ -9,8 +9,6 @@
 ;; Maintainer: Sebastian Ullrich <sebasti@nullri.ch>
 ;; Created: Jan 09, 2014
 ;; Keywords: languages
-;; Package-Version: 20210305.1705
-;; Package-Commit: 5c50338ac149ca5225fc737be291db1f63c45f1d
 ;; Package-Requires: ((emacs "24.3") (dash "2.18.0") (helm "2.8.0") (lean-mode "3.3.0"))
 ;; URL: https://github.com/leanprover/lean-mode
 
