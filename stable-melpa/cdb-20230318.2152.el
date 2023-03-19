65685a25f0541b188aae76157b3a82812007660c
Golem <golem@d12frosted.io>
2023-03-19 04:10:25 +0000

    snapshot 19/03/23 04:10:24

--- stable-melpa/cdb-20230318.2152.el
+++ stable-melpa/cdb-20230318.2152.el
@@ -5,8 +5,8 @@
 ;; Author: Yusuke Shinyama <yusuke at cs . nyu . edu>
 ;; Maintainer: SKK Development Team
 ;; URL: https://github.com/skk-dev/ddskk
-;; Package-Version: 20200904.1431
-;; Package-Commit: 2ce65eabea78d1eca10ae0ddf44f5769da1a36be
+;; Package-Version: 20230318.2152
+;; Package-Commit: 3820fa6bb0d53132aafb611a643c1e41e444052b
 ;; Keywords: cdb
 
 ;; This file is not part of GNU Emacs.
@@ -49,16 +49,16 @@
 (defun uint32le (x)
   "creates uint32le string from an integer."
   (string (% x 256)
-          (% (lsh x -8) 256)
-          (% (lsh x -16) 256)
-          (% (lsh x -24) 256)))
+          (% (ash x -8) 256)
+          (% (ash x -16) 256)
+          (% (ash x -24) 256)))
 
 (defun uint32le-int (v)
   "converts uint32le string v into an integer (if they can fit)."
   (logior (aref v 0)
-          (lsh (aref v 1) 8)
-          (lsh (aref v 2) 16)
-          (lsh (aref v 3) 24)))
+          (ash (aref v 1) 8)
+          (ash (aref v 2) 16)
+          (ash (aref v 3) 24)))
 
 (defun uint32le-int8l (v)
   "returns the lower 8 bits of the value."
@@ -67,8 +67,8 @@
 (defun uint32le-int24u (v)
   "returns the upper 8 bits of the value."
   (logior (aref v 1)
-          (lsh (aref v 2) 8)
-          (lsh (aref v 3) 16)))
+          (ash (aref v 2) 8)
+          (ash (aref v 3) 16)))
 
 (defun uint32le-eq (v1 v2)
   "returns true of the two uint32le values are equal."
@@ -93,13 +93,13 @@
 (defun uint32le-lsh (v n)
   "left-shifts the value v by n bits."
   (let ((a (string 0 0 0 0))
-        (x (lsh (aref v 0) n)))
+        (x (ash (aref v 0) n)))
     (aset a 0 (% x 256))
-    (setq x (logior (lsh (aref v 1) n) (/ x 256)))
+    (setq x (logior (ash (aref v 1) n) (/ x 256)))
     (aset a 1 (% x 256))
-    (setq x (logior (lsh (aref v 2) n) (/ x 256)))
+    (setq x (logior (ash (aref v 2) n) (/ x 256)))
     (aset a 2 (% x 256))
-    (setq x (logior (lsh (aref v 3) n) (/ x 256)))
+    (setq x (logior (ash (aref v 3) n) (/ x 256)))
     (aset a 3 (% x 256))
     a))
 
